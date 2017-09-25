d32b4ac7040ad931fcf3bfc63be2e7964740e0f0
paul pham <paul@aquaron.com>
2017-09-25 14:04:13 -0700

    First exe, fully functional

--- combine.pl
+++ combine.pl
@@ -3,28 +3,19 @@
 use strict;
 use PDF::API3::Compat::API2;
 
-printf "PDF: %s\n", &make_book2(@ARGV);
+&make_book(@ARGV);
 
 exit 0;
 
-sub make_book2 {
+sub make_book {
     my $i       = [
         { files => [split ',', (shift//q{})], pages => 0, current => 0 },
         { files => [split ',', (shift//q{})], pages => 0, current => 0 }
     ];
-    my $total   = 1;
-    my $file    = sprintf "temp-%s.pdf", time;
-    my $pdf     = PDF::API3::Compat::API2->new(-file => $file);
-
-    ## title
-    ## $pdf->cropbox(15,110,560,800);
-
-    ## odd
-    ## $pdf->cropbox(40,110,585,800);
-
-    ## $pdf->cropbox(25,110,570,800);
-
-    do {
+    my $total   = 0;
+    my $pdf     = PDF::API3::Compat::API2->new;
+    while ( scalar @{$i->[0]->{files}} || scalar @{$i->[1]->{files}} ||
+            $i->[0]->{pdf} || $i->[1]->{pdf}) {
         foreach my $oe (@$i) {
             my $files = $oe->{files};
             if (!$oe->{pdf} && scalar @$files) {
@@ -46,18 +37,20 @@ sub make_book2 {
                 undef $oe->{pdf};
             }
         }
-    } while (
-        scalar @{$i->[0]->{files}} ||
-        scalar @{$i->[1]->{files}} ||
-        $i->[0]->{pdf} ||
-        $i->[1]->{pdf}
-    );
+    }
 
-    eval { $pdf->save };
-    $@ and die "Cannot save $file: $!\n";
+    unless ($total) {
+        die "$0 odd-1.pdf,odd-2.pdf even-1.pdf,even-2.pdf,even-3.pdf\n";
+    }
 
-    print "Total pages $total\n";
-    return $file;
+    my $file    = sprintf "temp-%s.pdf", time;
+    eval { $pdf->saveas($file) };
+    if ($@) {
+        unlink $file;
+        die "Cannot save $file: $!\n";
+    }
+
+    print "$file has $total pages\n";
 }
 
 1;
