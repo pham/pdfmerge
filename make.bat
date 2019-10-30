6d6494bf365751ee651b3297084c10b9078e94d1
Paul Pham <paul@aquaron.com>
2019-10-30 09:04:20 -0700

    Add test to make.bat, restructure dirs

--- make.bat
+++ make.bat
@@ -1,23 +1,32 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
+SET PRODUCT=
+
+FOR /F "tokens=2 delims='" %%a IN (
+  'findstr /rc:" *PRODUCT *=> *" pdfmerge.pl'
+  ) DO ( SET "PRODUCT=%%a" )
+
+SET EXE=%PRODUCT%.exe
+
+IF (%1) == (test) GOTO TEST
+
 WHERE /q perlapp
 IF ERRORLEVEL 1 (
-    ECHO "perlapp not found, install"
-    EXIT /B
+  ECHO "perlapp not found, install"
+  EXIT /B
 )
 
 SET YEAR=
 SET VERSION=
+SET ICON=support\pdf.ico
 
 FOR /F "tokens=4 delims=/ " %%i IN ("%date%") DO SET "YEAR=%%i"
-FOR /F "tokens=2 delims=v" %%i IN ('findstr /rc:" *VERSION *=> *" pdfmerge.pl') DO (
+FOR /F "tokens=2 delims=v" %%i IN (
+    'findstr /rc:" *VERSION *=> *" pdfmerge.pl') DO (
   SET "tv=%%i"
   SET "VERSION=!tv:~0,3!"
 )
-
-SET EXE=pdfmerge.exe
-SET ICON=pdf.ico
 
 IF NOT EXIST "C:\Perl\bin" GOTO BIT64
 
@@ -28,9 +37,28 @@ IF NOT EXIST "C:\Perl64\bin" GOTO ABORT
 
 :START
 
-perlapp --icon %ICON% --norunlib --force --info "Comments=Zip merge PDFs given odd and even set of pages;CompanyName=Aquaron;FileDescription=Aquaron pdfmerge;FileVersion=%VERSION%;InternalName=pdfmerge %VERSION%;LegalCopyright=%YEAR% Aquaron;OriginalFilename=pdfmerge;ProductName=pdfmerge;ProductVersion=%VERSION%" --exe %EXE% pdfmerge.pl
+perlapp --icon %ICON% --norunlib --force --info "Comments=Zip merge PDFs given odd and even set of pages;CompanyName=Aquaron;FileDescription=Aquaron %PRODUCT%;FileVersion=%VERSION%;InternalName=%PRODUCT% %VERSION%;LegalCopyright=%YEAR% Aquaron;OriginalFilename=%EXE%;ProductName=%PRODUCT%;ProductVersion=%VERSION%" --exe %EXE% pdfmerge.pl
 
 ENDLOCAL
+
+EXIT /B
+
+:TEST
+
+IF EXIST %EXE% (
+  IF EXIST test (
+    CD test
+    IF EXIST odd.pdf ( IF EXIST even.pdf (
+      ..\%EXE% odd.pdf even.pdf
+      ECHO Check test dir for result
+      EXIT /B
+    ) )
+  )
+  ECHO Can't run test
+) ELSE (
+  ECHO Run make to build %EXE%
+  ECHO Or get from https://github.com/pham/pdfmerge/releases
+)
 
 EXIT /B
 
